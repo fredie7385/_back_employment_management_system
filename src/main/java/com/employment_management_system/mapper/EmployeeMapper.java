ee31278d69282d33226a657884cc983f6b25bced
Fredrick Kabiru <50455667+fredie7385@users.noreply.github.com>
2024-04-05 14:09:39 +0300

    simple spring bot crud app.

---
+++ src/main/java/com/employment_management_system/mapper/EmployeeMapper.java
@@ -0,0 +1,23 @@
+package com.employment_management_system.mapper;
+
+import com.employment_management_system.dto.EmployeeDto;
+import com.employment_management_system.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
---
+++ src/main/java/com/employment_management_system/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.employment_management_system.service;
+
+import com.employment_management_system.dto.EmployeeDto;
+
+import java.util.List;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+    EmployeeDto getEmployeeById(Long employeeId);
+    List<EmployeeDto> getAllEmployees();
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
+    void deleteEmployee(Long employeeId);
+}
